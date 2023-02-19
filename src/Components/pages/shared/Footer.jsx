073923f6227f3a5408361b0b9b568c4b214a335d
RedEye75 <yusufbinemamstk@gmail.com>
2023-02-20 01:47:35 +0600

    footer added

--- src/Components/pages/shared/Footer.jsx
+++ src/Components/pages/shared/Footer.jsx
@@ -1,9 +1,37 @@
 import React from "react";
-
+import bg from "../../../assets/images/footer.png";
 const Footer = () => {
   return (
     <div>
-      <h1>this is footer</h1>
+      <footer
+        style={{ background: `url(${bg})`, backgroundSize: "cover" }}
+        className="footer h-[407px] ml-36 mx-auto p-10 items-center "
+      >
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+      <div>
+        <p>Copyright © 2023 - All rights reserved </p>
+      </div>
     </div>
   );
 };
