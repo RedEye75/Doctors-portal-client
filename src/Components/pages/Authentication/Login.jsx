2d1d9f1405bce0cf2b71aa024bef682e11c23a7b
RedEye75 <yusufbinemamstk@gmail.com>
2023-03-05 21:19:03 +0600

    login page

--- src/Components/pages/Authentication/Login.jsx
+++ src/Components/pages/Authentication/Login.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h1>please login</h1>
+      <div>
+        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              {...register("email")}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              {...register("password")}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <p>{data}</p>
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   );
 };
